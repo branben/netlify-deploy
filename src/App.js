933e087159787c0f392ac6fac93e0e566a832d37
Brandon Bennett <107384180+codinghashira1120@users.noreply.github.com>
2022-11-14 18:57:40 -0500

    Removed unneccesary lines to make linter happy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-
-import './App.css';
-import Main from './components/Main';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "./App.css";
+import Main from "./components/Main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-
-
   return (
     <div className="App">
       <Router>
         <Routes>
-<Route path="/" element={<Main/>} />
-
-  </Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
       </Router>
- 
     </div>
   );
 }
